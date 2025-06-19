d5f568acd2373ea6ad362fff5bed4f0f7d13eaa2
Robert Ohuru <robertohuru@gmail.com>
2025-06-19 15:13:30 +0200

    Support using language specific config.json file

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ var terriaOptions = {
   baseUrl: "build/TerriaJS"
 };
 
+let _configUrl;
+
 import { runInAction } from "mobx";
 import ConsoleAnalytics from "terriajs/lib/Core/ConsoleAnalytics";
 import GoogleAnalytics from "terriajs/lib/Core/GoogleAnalytics";
@@ -15,11 +17,17 @@ import updateApplicationOnHashChange from "terriajs/lib/ViewModels/updateApplica
 import updateApplicationOnMessageFromParentWindow from "terriajs/lib/ViewModels/updateApplicationOnMessageFromParentWindow";
 import ViewState from "terriajs/lib/ReactViewModels/ViewState";
 import render from "./lib/Views/render";
+import loadJson5 from "terriajs/lib/Core/loadJson5";
 import registerCatalogMembers from "terriajs/lib/Models/Catalog/registerCatalogMembers";
 import registerSearchProviders from "terriajs/lib/Models/SearchProviders/registerSearchProviders";
 import defined from "terriajs-cesium/Source/Core/defined";
 import loadPlugins from "./lib/Core/loadPlugins";
 import plugins from "./plugins";
+import config from "./config.json";
+
+// keep track of the available languages for the currnet subdomain
+let _availableLanguages = ["en"];
+let _siteDefaultLanguage = "en";
 
 // Register all types of catalog members in the core TerriaJS.  If you only want to register a subset of them
 // (i.e. to reduce the size of your application if you don't actually use them all), feel free to copy a subset of
@@ -41,6 +49,8 @@ var terria = new Terria(terriaOptions);
 // insert your custom version of the code in the registerCustomComponentTypes function here instead.
 registerCustomComponentTypes(terria);
 
+setConfigForSubdomain();
+
 // Create the ViewState before terria.start so that errors have somewhere to go.
 const viewState = new ViewState({
   terria: terria
@@ -61,10 +71,26 @@ if (process.env.NODE_ENV !== "production" && module.hot) {
   document.styleSheets[0].disabled = true;
 }
 
+// this is to set language and reload page before bootstraping the app is completed, resulting  in a quicker page refresh without the user noticing it
+const langFromUrl = new URLSearchParams(location.search).get("lang");
+if (!langFromUrl) {
+  const userLang = localStorage.getItem("i18nextLng");
+  const params = new URLSearchParams(location.search);
+  // check if user default browser language is supported by the app if not fallback to site default language if not fallback to english
+  const lang = isSupportedLanguageOrDefault(userLang);
+  params.set("lang", lang);
+  window.location.search = params.toString();
+} else if (!isSupported(langFromUrl)) {
+  i18n.changeLanguage("en");
+  const params = new URLSearchParams(location.search);
+  params.set("lang", "en");
+  window.location.search = params.toString();
+}
+
 module.exports = terria
   .start({
     applicationUrl: window.location,
-    configUrl: "config.json",
+    configUrl: _configUrl,
     shareDataService: new ShareDataService({
       terria: terria
     }),
@@ -158,3 +184,60 @@ module.exports = terria
       console.error(e.stack);
     }
   });
+
+function isSupportedLanguageOrDefault(lang) {
+  const fallbackLanguage = "en";
+  const isSiteDefaultLangSupported = isSupported(_siteDefaultLanguage);
+  if (isSupported(lang)) {
+    return lang;
+  } else if (isSiteDefaultLangSupported) {
+    return _siteDefaultLanguage;
+  } else {
+    return fallbackLanguage;
+  }
+}
+
+function isSupported(lang) {
+  return _availableLanguages.includes(lang);
+}
+
+function setConfigForSubdomain() {
+  _configUrl = getConfigUrl();
+  loadJson5(_configUrl).then(function (config) {
+    document.title = config.parameters.appName;
+  });
+}
+
+function getConfigUrl() {
+  const url = new URL(window.location.href);
+  const isProd = config.main.prod.includes(url.hostname);
+  const sites = isProd ? config.sites.apps : config.sites.review;
+  const paths = url.pathname.split("/").slice(1);
+  const urlPath =
+    paths.length !== 0
+      ? paths.filter(Boolean).join("/") // This is to allow second level context paths, but first filter empty strings
+      : "default";
+  const _site = sites.hasOwnProperty(urlPath) ? urlPath : "default";
+  const siteConfig = sites[_site];
+  _availableLanguages = config.languages[_site].supportedLangs;
+  _siteDefaultLanguage = config.languages[_site].defaultLang;
+  const lang = getLanguage();
+  const site_with_translation = loadConfig(lang, siteConfig);
+  return site_with_translation;
+}
+
+function getLanguage() {
+  const urlParams = new URLSearchParams(window.location.search);
+  const lang = urlParams.get("lang");
+  return lang;
+}
+
+function loadConfig(lang, defaultConfig) {
+  const fallbackLang = "en";
+  const configUrl =
+    lang === fallbackLang || !lang
+      ? defaultConfig
+      : defaultConfig.replace(".json", `.${lang}.json`);
+
+  return configUrl;
+}
